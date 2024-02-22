fc0e0e53f213235435167015de21227f85dc7106
aeschi <anna.eschenbacher@icloud.com>
2024-02-22 14:14:47 +0100

    fix: add english typeform link and clean up

--- src/components/feedback/index.tsx
+++ src/components/feedback/index.tsx
@@ -1,25 +1,22 @@
 import { t } from "../../i18n/translations";
 import { useI18nStore } from "../../i18n/store";
-import { availableLanguages } from "../../../src/i18n/store/types";
 
 export function Feedback() {
   const language = useI18nStore((state) => state.language);
 
   return (
     <>
-      {language === availableLanguages[0] && (
-        <div className="flex w-full flex-wrap justify-center gap-x-2 px-8 py-4 text-sm">
-          {t("feedback.question", language)}
-          <a
-            className="text-blue-700 underline"
-            href={t("feedback.link", language)}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {t("feedback.link.label", language)}
-          </a>
-        </div>
-      )}
+      <div className="flex w-full flex-wrap justify-center gap-x-2 px-8 py-4 text-sm">
+        {t("feedback.question", language)}
+        <a
+          className="text-blue-700 underline"
+          href={t("feedback.link", language)}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {t("feedback.link.label", language)}
+        </a>
+      </div>
     </>
   );
 }
