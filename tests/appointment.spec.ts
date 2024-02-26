66c12a05b30813160e6febc49700a9a28af739e2
Raphael Arce <raphael.arce@ts.berlin>
2024-02-26 17:20:50 +0100

    chore: fix failing test in webkit
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- tests/appointment.spec.ts
+++ tests/appointment.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "@playwright/test";
 
 test("Should read appointment from URL query params", async ({ page }) => {
-  await page.goto("http://localhost:5173/?appointment=2024-02-28Z15:30:00");
+  await page.goto("http://localhost:5173/?appointment=2024-02-28T15:30:00Z");
 
   const appointmentParagraph = page.getByText("Dein Termin:Mittwoch, 28.");
   await expect(appointmentParagraph).toBeVisible();
