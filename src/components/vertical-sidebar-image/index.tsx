d5733ab286bf4716d07bd6389affd77aeff9dc46
Raphael Arce <raphael.arce@ts.berlin>
2024-02-26 18:05:04 +0100

    feat: make sidebar image responsive with html picture, srcset and media query
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- src/components/vertical-sidebar-image/index.tsx
+++ src/components/vertical-sidebar-image/index.tsx
@@ -1,33 +1,18 @@
 export function VerticalSidebarImage() {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <picture>
+      <picture className="h-full">
         <source
-          className="h-full object-cover"
-          media="(min-width: 1080px)"
+          media="(min-width:1000px)"
           srcSet="/images/fernsehturm_alpha_paperplane.png"
         />
-        <source srcSet="/images/fernsehturm_alpha_paperplane_slim.png" />
-        {/* <img
+        <img
+          src="/images/fernsehturm_alpha_paperplane_slim.png"
+          alt="Flowers"
           className="h-full object-cover"
-          src="/images/fernsehturm_alpha_paperplane.png"
-          alt="Bild des Berliner Fernsehturms"
           loading="lazy"
-        /> */}
+        />
       </picture>
-
-      {/* <img
-        src="/images/fernsehturm_alpha_paperplane.png"
-        className="h-full object-cover md:hidden lg:flex"
-        alt="Bild des Berliner Fernsehturms"
-        loading="lazy"
-      />
-      <img
-        src="/images/fernsehturm_alpha_paperplane_slim.png"
-        className="h-full object-cover lg:hidden"
-        alt="Bild des Berliner Fernsehturms"
-        loading="lazy"
-      /> */}
     </div>
   );
 }
