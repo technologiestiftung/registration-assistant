f9f71696a8b3b20b52ab14821a761664b206c91e
aeschi <anna.eschenbacher@icloud.com>
2024-02-26 17:14:31 +0100

    fix: add srcset still WIP

--- src/components/vertical-sidebar-image/index.tsx
+++ src/components/vertical-sidebar-image/index.tsx
@@ -1,7 +1,22 @@
 export function VerticalSidebarImage() {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <img
+      <picture>
+        <source
+          className="h-full object-cover"
+          media="(min-width: 1080px)"
+          srcSet="/images/fernsehturm_alpha_paperplane.png"
+        />
+        <source srcSet="/images/fernsehturm_alpha_paperplane_slim.png" />
+        {/* <img
+          className="h-full object-cover"
+          src="/images/fernsehturm_alpha_paperplane.png"
+          alt="Bild des Berliner Fernsehturms"
+          loading="lazy"
+        /> */}
+      </picture>
+
+      {/* <img
         src="/images/fernsehturm_alpha_paperplane.png"
         className="h-full object-cover md:hidden lg:flex"
         alt="Bild des Berliner Fernsehturms"
@@ -12,7 +27,7 @@ export function VerticalSidebarImage() {
         className="h-full object-cover lg:hidden"
         alt="Bild des Berliner Fernsehturms"
         loading="lazy"
-      />
+      /> */}
     </div>
   );
 }
