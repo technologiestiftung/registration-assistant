ca4748dbd889263582fbdfa67288f2467f39406e
Raphael Arce <raphael.arce@ts.berlin>
2024-04-16 12:14:38 +0200

    feat: add matomo intro page goal
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- src/components/forms/intro/hooks/use-save-intro-page-view-in-session-storage.tsx
+++ src/components/forms/intro/hooks/use-save-intro-page-view-in-session-storage.tsx
@@ -1,7 +1,18 @@
 import { useEffect } from "react";
+import { trackInteraction } from "../../../feedback/matomo.ts";
+import { useI18nStore } from "../../../../i18n/store";
 
 export function useSaveIntroPageViewInSessionStorage() {
-  useEffect(saveIntroPageViewInSessionStorage, []);
+  const { language } = useI18nStore();
+
+  useEffect(() => {
+    saveIntroPageViewInSessionStorage();
+
+    trackInteraction({
+      eventAction: "page-view",
+      eventName: `intro-page-view (language: ${language})`,
+    });
+  }, []);
 }
 
 function saveIntroPageViewInSessionStorage() {
